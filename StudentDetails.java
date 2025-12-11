0e9315e5d5b80ef456e21777b9ac1cadc567c430
kl2400032177 <rajarajeswariguntupalli2417@gmail.com>
2025-12-11 21:49:09 +0530

    Updated Java code

--- StudentDetails.java
+++ StudentDetails.java
@@ -7,26 +7,35 @@ public class StudentDetails {
 
         int id;
         String name;
-        String sem;
+        int sem;
         double sgpa;
 
         System.out.print("Enter Student ID: ");
         id = sc.nextInt();
-        sc.nextLine();  
+        sc.nextLine();  // Clear buffer
 
         System.out.print("Enter Student Name: ");
         name = sc.nextLine();
 
-        System.out.print("Enter Semester(even or odd): ");
-        sem = sc.nextLine();
+        // Accept only 1st year even semester (Semester 2)
+        while (true) {
+            System.out.print("Enter Semester (Only 2nd Sem allowed): ");
+            sem = sc.nextInt();
+
+            if (sem == 2) {
+                break; // Valid semester
+            } else {
+                System.out.println("❌ Invalid! 1st year even sem = Semester 2 only. Try again.");
+            }
+        }
 
         System.out.print("Enter SGPA: ");
-        cgpa = sc.nextDouble();
+        sgpa = sc.nextDouble();
 
         System.out.println("\n--- Student Details ---");
         System.out.println("ID: " + id);
         System.out.println("Name: " + name);
-        System.out.println("Semester: " + sem);
+        System.out.println("Semester: " + sem + " (1st Year Even Sem)");
         System.out.println("SGPA: " + sgpa);
     }
 }
