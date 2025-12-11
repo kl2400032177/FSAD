9620b81b58701dcd49a23ba00f998d1b591cea4f
Raja Rajeswari Guntupalli <rajarajeswariguntupalli2417@gmail.com>
2025-12-11 22:05:39 +0530

    Add files via upload

---
+++ 2400032177_FSAD/Student.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class StudentDetails {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int id;
+        String name;
+        String sem;
+        double cgpa;
+
+        System.out.print("Enter Student ID: ");
+        id = sc.nextInt();
+        sc.nextLine();  
+
+        System.out.print("Enter Student Name: ");
+        name = sc.nextLine();
+
+        System.out.print("Enter Semester: ");
+        sem = sc.nextLine();
+
+        System.out.print("Enter CGPA: ");
+        cgpa = sc.nextDouble();
+
+        System.out.println("\n--- Student Details ---");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Semester: " + sem);
+        System.out.println("CGPA: " + cgpa);
+    }
+}
